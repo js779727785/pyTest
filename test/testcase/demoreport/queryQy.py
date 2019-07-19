3f157e5996c2378640749d357aa15d20f3ea0c3c
js779727785 <>
2019-07-19 15:55:35 +0800

    fix queryQY

--- test/testcase/demoreport/queryQy.py
+++ test/testcase/demoreport/queryQy.py
@@ -17,10 +17,9 @@ JD查询企业信息
 class queryQy(unittest.TestCase):
     """JD查询企业信息---Jmelody"""
     def setUp(self):
-        logger.info("*" * 80)
+        logger.info("****************JD查询企业信息接口开始****************")
 
     def getTest(self, tx):
-        logger.info("****************JD查询企业信息接口开始****************")
         infoUrl = "/jd/credit/getEntBaseInfo"
         caseNum = tx['test_name']
         caseName = tx['test_description']
@@ -29,7 +28,7 @@ class queryQy(unittest.TestCase):
         platform =tx['platform']
         code = tx['code']
         logger.info("*******测试案例名称： TestCase" + caseNum + "_" + caseName + " 执行开始********")
-        if caseNum == '002':
+        if caseNum == '003':
             enterpriseName=''
         logger.info("*******测试数据： " + str(enterpriseName))
         test_url=dfb_url+infoUrl+"?enterpriseName="+enterpriseName+"&ssoId="+ssoId+"&platform="+platform
@@ -40,7 +39,10 @@ class queryQy(unittest.TestCase):
         logger.info("*******requests结果code:  "+str(result['code'])+"     ,预期结果： "+tx['code'])
         self.assertEqual(code, str(result['code']))
         logger.info("*******测试案例名称： TestCase" + caseNum + "_" + caseName + " 执行完毕********")
-        logger.info("****************JD查询企业信息接口结束****************")
+        if result['code']==10000:
+            print("取得企业status内容:   "+result['data']['basicInfo']['entStatus'])
+            logger.info("取得企业status内容:   "+result['data']['basicInfo']['entStatus'])
+
 
     @staticmethod
     def getTestFunc(arg1):
@@ -49,6 +51,6 @@ class queryQy(unittest.TestCase):
         return func
 
     def tearDown(self):
-        logger.info("*" * 80)
+        logger.info("****************JD查询企业信息接口结束****************")
 
 __generateTestCases(queryQy, "queryQy", "demo.xlsx", "JD查询企业信息")
