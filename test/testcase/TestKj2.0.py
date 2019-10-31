887dc7577678515449d57d320174235668cb815e
Jmelody <js779727785@qq.com>
2019-10-31 15:19:28 +0800

    优化新的脚本框架模板

--- test/testcase/TestKj2.0.py
+++ test/testcase/TestKj2.0.py
@@ -3,43 +3,52 @@ import unittest
 import requests
 from lib.generateTestCases import __generateTestCases
 from lib.log import logger
-from lib.MySQLHelper import MySQLHelper
-
-headers = {"Content-type": "application/json"}
+headers = {"Content-Type": "application/json"}
 
 """
-复制模板
+统一风格的模板，author：Jmelody
+文件命名：api_post_qyd_http_nbigfront_entrance_newmt_XX_json.py
+接口文档：
+getZyBackMoneyPlan
+api_post_qyd_http_nbigfront_web_accessionlist_json.py
 """
-
-class checkUserRole(unittest.TestCase):
-    """demoKJ------Jmelody"""
+class InstanseName(unittest.TestCase):
+    """demo接口-Jmelody"""
     def setUp(self):
         logger.info("*" * 80)
 
     def getTest(self, tx):
-        logger.info("****************接口开始****************")
-        infoUrl = "/entrance/qyduser/checkUserRole"
-        caseNum = tx['test_name']
+        logger.info("****************demo接口开始****************")
+        infoUrl = qyd_base_url + "/nbigfront/"
+        caseNum = tx['tc_num']
         caseName = tx['test_description']
-        someParms = tx['someParms']
-        status = tx['status']
-        successful = tx['successful']
-        data={"someParms":someParms}
+        tel_num = tx['tel_num']
+        password = tx['password']
+        productType = tx['productType']
+        flag=tx['flag']
+        code = int(tx['code'])
+        token = qydFrontLogin(tel_num, password)
+        headers = {"Content-Type": "application/json", "X-Auth-Token": token}
+        data = {"productType": productType}
         logger.info("*******测试案例名称： TestCase" + caseNum + "_" + caseName + " 执行开始********")
-        reponse=requests.post(url=infoUrl,json=data,headers=headers)
-        reponse_json=reponse.json()
-        logger.info("请求接口返回结果:"+str(reponse_json))
-        self.assertEqual(reponse_json['entities'][0]['status'],status)
+        logger.info("请求参数：" + str(data) + "headers:" + str(headers))
+        reponse = requests.post(url=infoUrl, json=data, headers=headers)
+        reponse_json = reponse.json()
+        # print(reponse_json)
+        logger.info("请求接口返回结果:" + str(reponse_json))
+        self.assertEqual(reponse_json['code'], code)
         logger.info("*******测试案例名称： TestCase" + caseNum + "_" + caseName + " 执行完毕********")
-        logger.info("****************接口结束****************")
+        logger.info("****************demo接口结束****************")
 
     @staticmethod
     def getTestFunc(arg1):
         def func(self):
             self.getTest(arg1)
+
         return func
 
     def tearDown(self):
         logger.info("*" * 80)
 
-__generateTestCases(checkUserRole, "checkUserRole", "caseExcel.xlsx", "sheetName")
+
+__generateTestCases(InstanseName, "instanseName", "api_qyd.xlsx", "sheetName")
