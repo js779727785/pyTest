a2e80497c6587854acb97634c39573dacc20108a
Jmelody <js779727785@qq.com>
2019-11-06 11:57:39 +0800

    1.增加一些有趣的算法

--- test/testcase/testgo.py
+++ test/testcase/testgo.py
@@ -5,6 +5,25 @@ from lib.mysql.divestmentAndBuy import queryAssign,queryTranaction
 from lib.log import logger
 import requests,random
 from config.url import qydregisterPhotoUrl,qydregisterSendMsgUrl,qydregisterUrl
+from lib.MySQLHelper import MySQLHelper
+"""斐波那契数列"""
+def func(n):
+    if n==1 or n==2:
+        return 1
+    else:
+        return func(n-1)+func(n-2)
+
+a={1,2,3,44,55,22,3,4,22}
+"""冒泡排序并统计次数"""
+def func(a):
+    count=0
+    for i in range(len(a)-1):
+        for j in range(len(a)-i-j):
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
+                count+=1
+    return a,count
+
 def testgo():
     """二者之间的随机数"""
     # str(random.randrange(10000000, 99999999))
@@ -26,12 +45,12 @@ def demomanysql(param):
     possLen = possessionInfo.__len__()
     print(possLen)
 
-def checkGetMany1():
+def checkGetMany1(self):
     """第一种get_many结果的断言语句可以直接拿返回结果，用checkPossessionData判断"""
     items=requests(url="",json="",header="")
     resultInfo=queryAssign
     self.checkPossessionData(resultInfo, items)
-def checkGetMany2():
+def checkGetMany2(self):
     """第二种get_many在返回结果前在sql中for循环取结果"""
     items = requests(url="", json="", header="")
     resultInfo = queryAssign
@@ -61,4 +80,71 @@ def imgCheck():
     imgre=requests.post(url=url_info,json=imgParm)
     print(imgre)
     logger.info("图形验证码返回结果:"+str(imgre))
-imgCheck()
+
+
+def queryConfireLoan(self):
+    sql_info = "select id from qydproduction.mt_loan where STATUS=%s order by create_time desc limit 2;"
+    response = MySQLHelper('qydproduction').get_many(sql_info,"CONFIRM")
+    loanId=response[0]['id']
+    print(loanId)
+
+def checkType(self,tel_num,product_type):
+     sql_info="SELECT * FROM qydnewproduction.mt_reinvestment WHERE user_id=(SELECT id from qydproduction.user where tel_num={}) and product_type='{}';".format(tel_num,product_type)
+     response=MySQLHelper("qydnewproduction").selectsql(sql_info)
+     rep=response[0]['product_type']
+     res=response[0]['status']
+     logger.info("a:{},B:{}".format(rep,res))
+"""快速排序"""
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    pivot = arr[0]
+    less = [i for i in arr[1:] if i < pivot]
+    greater = [i for i in arr[1:] if i > pivot]
+    return quick_sort(less) + [pivot] +  quick_sort(greater)
+
+a = [23,33,44,12,55,12,8]
+print(quick_sort(a))
+
+#老式快速排序
+# def partition(arr, low, high):
+#     i = (low - 1)  # 最小元素索引
+#     pivot = arr[high]
+#     for j in range(low, high):
+#         # 当前元素小于或等于 pivot
+#         if arr[j] <= pivot:
+#             i = i + 1
+#             arr[i], arr[j] = arr[j], arr[i]
+#     arr[i + 1], arr[high] = arr[high], arr[i + 1]
+#     return (i + 1)
+# # arr[] --> 排序数组
+# # low  --> 起始索引
+# # high  --> 结束索引
+# # 快速排序函数
+# def quickSort(arr, low, high):
+#     if low < high:
+#         pi = partition(arr, low, high)
+#         quickSort(arr, low, pi - 1)
+#         quickSort(arr, pi + 1, high)
+# arr = [10, 7, 8, 9, 1, 5]
+# n = len(arr)
+# quickSort(arr, 0, n - 1)
+# print("排序后的数组:")
+# for i in range(n):
+#     print("%d" % arr[i])
+#
+# def quick_sort(lis):
+#     if len(lis)<2:
+#         return lis
+#     else:
+#         tem=lis[0]
+#         max= [i for i in range(lis[1:]) if i>tem]
+#         less=[i for i in range(lis[1:]) if i<tem ]
+#     return quick_sort(less)+[tem]+quick_sort(max)
+"""九九乘法表"""
+def jiujiu():
+    for i in range(1,10):
+        for j in range(1,i+1):
+            print("{}*{}={}\t".format(j,i,i*j),end="")
+        print()
+jiujiu()
