6d327a2268fc5f48040afd2cfd3be12abeb32925
jingshuai <779727785@qq.com>
2019-11-12 09:00:44 +0800

    排序相关

--- test/testcase/testgo.py
+++ test/testcase/testgo.py
@@ -4,7 +4,7 @@ from lib.mysql.userQuery import queryOldPossessionInfo
 from lib.mysql.divestmentAndBuy import queryAssign,queryTranaction
 from lib.log import logger
 import requests,random
-from config.url import qydregisterPhotoUrl,qydregisterSendMsgUrl,qydregisterUrl
+import operator
 from lib.MySQLHelper import MySQLHelper
 """斐波那契数列"""
 def func(n):
@@ -58,6 +58,7 @@ def checkGetMany2(self):
     if resultInfo is not None or resultInfo.__len__()>0:
         for i in range(resultInfo.__len__()):
             self.assertEquals(resultInfo[i]["sqlStatus"], testdata[0]['exceptStatus'])
+
 def gogo():
     """用python编程，从屏幕上输入一个字符串，将这个字符串里面所有不同的字符提取出来，放在一个字符数组里面"""
     str = 'today I will go home'
@@ -226,11 +227,49 @@ set1.clear()
 # for i in set1:
     # print(i)
 
-"""两个list去重之后取交集，返回list"""
+"""两个list去重之后取交集，返回list,注意返回是无序的，用sorted(list)进行排序或list.sort()"""
 def setDemo(lis1,list2):
     lis= list(set(lis1)^set(list2))
     print(lis)
     return lis
-a=[1,2,3,4,5]
-b=[6,5,4,2]
+a=[1,2,3,666,4,5]
+b=[999,6,5,4,2,999,4,2,4,5,666,2]
 # setDemo(a,b)
+"""反交集并排序"""
+def setDemo2(lis1,lis2):
+    lis= sorted(list(set(lis1)^set(lis2)))
+    print(lis)
+    return lis
+# setDemo2(a,b)
+"""对数组元素出现个数计数"""
+def countNum(lis,strValue):
+    num=str(lis).count(strValue)
+    print(num)
+    return num
+"""todo{}集合如何计数？？？？"""
+# num=str(b).count('2')
+# num=b.count(2)
+# print(num)
+# countNum(b,'2')
+
+
+"""python将一个list去重并按出现次数从大到小排序"""
+list_a = [1,2,3,4,5,6,1,1,1,2,2,3,4,4,5,5,5,5,6,6,6,6,6,6]
+print("list_a: " + str(list_a) + '\n')
+
+set_a = list(set(list_a))  # 去重得到一个集合
+print("set_a: " + str(set_a) + '\n')
+
+count_set_a = {}  # 存放元素和出现次数的字典，key为元素,value为出现次数
+for item in set_a:
+    count_set_a[item] = list_a.count(item)
+print("count_set_a: " + str(count_set_a) + '\n')
+
+# 将cou_set_a按value值排序，返回一个list，list中元素是形式为(1,4)的tuple，tuple[0]为键值，tuple[1]为出现次数
+sorted_list_a = sorted(count_set_a.items(), key=operator.itemgetter(1))
+print("sorted_list_a: " + str(sorted_list_a) + '\n')
+
+result_a = []  # 存放最后的结果
+for item in sorted_list_a[::-1]: # 按value值从大到小排序
+    result_a.append(item[0])
+print("result_a: " + str(result_a) + '\n')
